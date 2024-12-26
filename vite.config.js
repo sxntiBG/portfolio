cc4aae523b96d5f43bb05307bb327b851de79cec
SxntiBG <zapataospinasantiago8@gmail.com>
2024-12-26 14:28:03 -0500

    update Vite configuration; set base URL for deployment; add predeploy and deploy scripts in package.json; update asset paths in index.html

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Si usas React, ajusta según tu caso
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '/assets': '/src/assets',
-    },
-  },
-});
+  base: 'https://sxntiBG.github.io/portfolio',
+})
