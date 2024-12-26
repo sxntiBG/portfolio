8e8a7f716f6d8690b140f98f149f64fe7f60cc2f
SxntiBG <zapataospinasantiago8@gmail.com>
2024-12-26 16:06:54 -0500

    update index.html title and script source; modify vite.config.js base path and build output options

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://sxntiBG.github.io/portfolio',
+  base: '/', 
   build: {
     outDir: 'dist',
-  }
-})
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'], 
+});
