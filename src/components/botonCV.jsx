3b6948d0f7375cb8beab9be8e39e5f13bcef999c
SxntiBG <zapataospinasantiago8@gmail.com>
2025-03-07 15:15:20 -0500

    Actualización CV

--- src/components/botonCV.jsx
+++ src/components/botonCV.jsx
@@ -5,7 +5,7 @@ export default function BotonCV() {
 
   return (
     <div className="relative">
-      <a href="./assets/CV/CV-SantiagoZapataOspina.pdf" 
+      <a href="./assets/CV/CV - Santiago Zapata Ospina.pdf" 
         download>
       <button 
         className="bg-[#a987c9] text-white px-8 py-2 rounded-full mb-2 transition-transform transform hover:font-bold hover:scale-105 hover:bg-[#915bb8] duration-300 ease-in-out flex items-center gap-2"
